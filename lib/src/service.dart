a698a2a0d4d50bcbce24b6dd3987f49bbc0ced4d
Rithik Bhandari <rithikbhandari895@gmail.com>
2021-06-26 14:16:56 +0530

    removed unused import

--- lib/src/service.dart
+++ lib/src/service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_notifications_handler/src/app_state.dart';
 import 'package:flutter/cupertino.dart' show GlobalKey, NavigatorState;
