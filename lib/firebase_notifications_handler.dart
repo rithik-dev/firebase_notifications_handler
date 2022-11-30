6d03a614d1c2a0af5097ca5024da966581c8c6db
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-11-30 19:57:41 +0530

    feat: added flutter_local_notifications as transitive dep

--- lib/firebase_notifications_handler.dart
+++ lib/firebase_notifications_handler.dart
@@ -1,6 +1,7 @@
 library firebase_notifications_handler;
 
 export 'package:firebase_messaging/firebase_messaging.dart';
+export 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 export 'src/app_state.dart';
 export 'src/widget.dart';
