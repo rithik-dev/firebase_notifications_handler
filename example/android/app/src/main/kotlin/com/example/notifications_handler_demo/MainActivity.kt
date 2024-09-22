d834d7ebc4c2a8abe99ada3c6f7fcc2eab1ee1b6
Rithik Bhandari <56810766+rithik-dev@users.noreply.github.com>
2024-09-23 00:01:03 +0530

    feat: updated android example app

--- example/android/app/src/main/kotlin/com/example/notifications_handler_demo/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/notifications_handler_demo/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.notifications_handler_demo
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
